2bde7ceb830a6e192117ef27f053f774da297497
Jeff Conn <jeff.conn121@gmail.com>
2022-02-07 20:04:51 -0500

    Level-2 Completion of Mario Pest Control

---
+++ Level-2/Mario-Pest-Control/main.js
@@ -0,0 +1,19 @@
+const myForm = document.myForm
+
+myForm.addEventListener("submit",(e) =>{
+    e.preventDefault()
+    let goombas = myForm.goombas.value
+    let bombs = myForm.bobOmbs.value
+    let cheeps = myForm.cheepCheeps.value
+   
+    let result = myForm.result.value
+
+    function sum(x, y, z){
+        return Number(x*5) + Number(y*7) + Number(z*11)
+    }
+
+    myForm.result.value = sum(goombas, bombs, cheeps) 
+
+    console.log(result)
+    
+})
