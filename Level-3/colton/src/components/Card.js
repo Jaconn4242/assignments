8b6399327412bbb6e91dfd9a8811f8cc3148e036
Jeff Conn <jeff.conn121@gmail.com>
2022-03-04 12:53:19 -0500

    Travel Journal update

--- Level-3/colton/src/components/Card.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function Card(){
-    return(
-        <h1>Hello World, Lets do this!</h1>
-    )
-}
---
+++ Level-3/travel-journal/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <div className="Header--header">
+            <img src="https://img.icons8.com/material/50/000000/around-the-globe.png" className="Header--logo"/>
+            <p className="Header--title">my travel journal.</p>
+        </div>
+    )
+}
