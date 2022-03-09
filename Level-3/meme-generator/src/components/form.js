e8f633eddc1b55eafd9006d570d5e7b220f2dccc
Jeff Conn <jeff.conn121@gmail.com>
2022-03-09 09:20:50 -0500

    UseState Practice Complete

--- Level-3/meme-generator/src/components/form.js
+++ Level-3/meme-generator/src/components/form.js
@@ -1,13 +1,30 @@
 import React from "react"
+import memesData from "../memesData"
+
+
 
 export default function Form(){
+    const [memeImage, setMemeImage] = React.useState("")
+    
+
+
+
+    function getMemeImage(e){
+        e.preventDefault()
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        console.log(setMemeImage)
+    }
     return (
         <main>
             <form className="Form">
                 <input className="Form--input" type="text" placeholder="Top Text"></input>
                 <input className="Form--input" type="text" placeholder="Bottom Text"></input>
-                <button className="Form--button">Get a new meme image</button>
+                <button onClick={getMemeImage} className="Form--button">Get a new meme image</button>
             </form>
+            <img src={memeImage} className="Form--meme-image"></img>
         </main>
     )
 }
+
---
+++ Level-3/usestate-practice/src/App.js
@@ -0,0 +1,94 @@
+import React, {useState} from "react"
+
+export default function App() {
+
+  const [person, setPerson] = React.useState({
+    firstName: "John",
+    lastName: "Smith"
+  });
+
+const age = {age: 30}
+
+function addKvp(){
+setPerson(prevState => [...prevState + {...age}])
+}
+  
+  return (
+    <>
+    <button onClick={addKvp}>Make things Happen</button>
+    <h1>{person}</h1>
+    </>
+  );
+}
+
+
+
+
+
+
+//6.     What’s wrong with the following state update?
+// const [colors, setColors] = useState(["pink", "blue"])
+  
+  // function handleClick(){
+  //   setColors(prevColors => [...prevColors, "green"])
+  // }
+
+
+
+
+
+
+// 5.    Update the following state to add an additional property age and set the value to 30, ensuring that the other properties are not overwritten.
+
+
+
+
+
+
+
+// 4.    Change the following state-setting functions to use an implicit return
+// a.
+//   const [colors, setColors] = React.useState(["pink ", "blue"])
+
+// function doThis() {
+  //   setColors(prevColors => [...prevColors, " green"])
+  // }
+
+// b.
+//   const [count, setCountObject] = React.useState(0)
+  
+//   function increment(){
+//     setCountObject(count => count + 1)
+//   }
+  
+  
+  
+  //2.    Toggle between the colors pink and blue. If the current color is pink, we want to set it to blue. If it’s currently blue, set it back to pink.
+  // const [color, setColor] = React.useState("")
+
+// function handleClick(){
+  //   setColor("your not")
+  // }
+  // function handleAgain(){
+    //   setColor("just hit refresh haha")
+// }
+
+
+
+
+// 3.    Add a new person object to the following people array in state. You can hard-code a new object but it must be a person object with firstName and lastName properties.
+// const [people, setPeople] = React.useState([
+  // 	  {
+    // 		  firstName: "John",
+    // 		  lastName: "Smith"
+    // 	  }
+    //   ])
+// const friend = {
+//   firstName: "Colton",
+//   lastName: "Walker"
+// }
+
+//   function addPeople() {
+//     setPeople(people.concat(friend))
+//   }
+//   console.log(people)
