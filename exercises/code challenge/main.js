a177b873fcfab0306f4fdd24f4f7144404eea5e2
Jeff Conn <jeff.conn121@gmail.com>
2022-03-24 13:52:44 -0400

    completed Jordan's award

--- exercises/code challenge/main.js
+++ exercises/code challenge/main.js
@@ -1,23 +1,15 @@
-// Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
-// Examples
-// filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+const array = ["#3", "$$$", "C%4!", "Hey!", "!"];
 
-// filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
-
-// filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
-
-
-const grabNumbers = (arr) => {
-
-    let results = arr.filter(item => {
-        if(Number.isInteger(item)){
-            return item
-        }
-    })
-    return results
+function findCharacters(array, character) {
+    const charArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].includes(character)) {
+      charArray.push(array[i])
+    }
+  }
+  return charArray
 }
-
-console.log(grabNumbers([1, 2, "a", "b", 44]))
+console.log(findCharacters(array, "!"));
 
 
 
