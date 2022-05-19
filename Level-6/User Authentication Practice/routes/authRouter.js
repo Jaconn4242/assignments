36ac46445ff0827ee0e6fceeafc8dcac8b68c193
Jeff Conn <jeff.conn121@gmail.com>
2022-05-19 17:25:58 -0400

    add gitignore file

--- Level-6/User Authentication Practice/routes/authRouter.js
+++ Level-6/User Authentication Practice/routes/authRouter.js
@@ -26,6 +26,26 @@ authRouter.post("/signup", (req,res,next) => {
     })
 })
 
+authRouter.post("/login",(req, res, next) => {
+    User.findOne({username: req.body.username}, (err, user) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        if(!user){
+            res.status(403)
+            return next(new Error("Username or Password are incorrect"))
+        }
+        if(req.body.password !== user.password){
+            res.status(403)
+            return next(new Error("Username or Password are incorrect"))
+        }
+
+        const token = jwt.sign(user.toObject(), process.env.SECRET)
+        return res.status(200).send({token, user})
+    })
+})
+
 
 
 
